a61a86690f8a3231a355b9e9c4db74a50f28fe0f
Trista Cheng <trista8130@gmail.com>
2021-06-30 13:10:14 -0400

    init commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//Mount function to start up the app
+const mount = (ele) => {
+  ReactDOM.render(<App />, ele);
+};
+
+//If we are in development and in isolation,
+//call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+//We are running through container
+//and we should export the mount function
+export { mount };
